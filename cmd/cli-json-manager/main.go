ce8c25d0af3def2abf8ca841e2c334bce6289ac1
thissidemayur <thissidemayur@gmail.com>
2025-11-01 15:49:54 +0530

    add error handling

--- cmd/cli-json-manager/main.go
+++ cmd/cli-json-manager/main.go
@@ -9,77 +9,107 @@ import (
 )
 
 
-func main(){
-	// CLI Commands
-	addCmd := flag.NewFlagSet("add" , flag.ExitOnError)
-	listCmd := flag.NewFlagSet("list",flag.ExitOnError)
-	deleteCmd := flag.NewFlagSet("delete",flag.ExitOnError)
-	updateCmd := flag.NewFlagSet("update",flag.ExitOnError)
+func main() {
+	// global flag share across all commands
+	fileFlag := flag.String("file","data.json","JSON file to store data (default: data.json)")
 
-	// shared flag across commands
-	fileFlag := "data.json"
+	// parse only top-level (global) flags so user can do: cli-json-manager --file=user.json add...
+	flag.Parse();
 
-
-	// add command flags
-	name:= addCmd.String("name","","Name to add")
-	addFile := addCmd.String("file",fileFlag,"JSON file to store data")
-
-	// list command flags
-	listFile := listCmd.String("file",fileFlag,"JSON file to store data")
-
-	// delete command flags
-	deleteFile := deleteCmd.String("file",fileFlag,"JSON file to store data")	
-	deleteId := deleteCmd.Int("id",0,"Id to delete")
-
-	// update command flags
-	updateFile := updateCmd.String("file",fileFlag,"JSON file to store data")
-	updateId := updateCmd.Int("id",0,"Id to update")
-	updateName := updateCmd.String("name","","New Name")
-
-	// ensure subcommand are provided
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: cli-json-manager <command> [options] <value>")
+	// ensure subcommand are provided like add, list, delete, update
+	if flag.NArg() < 1 {
+		fmt.Println("❌ Missing command.")
+		fmt.Println("Usage: cli-json-manager [--file <fileName] <command> [options]")
 		fmt.Println("Commands: add, list, delete, update")
-		return
+		os.Exit(1)
 	}
 
-	// switch between command
-	switch os.Args[1]{
+	// first positional argument- subcommand (add, list, delete, update)
+	cmd := flag.Arg(0);
+
+	switch cmd {
+		// ---------------- ADD ----------------
 	case "add":
-		addCmd.Parse(os.Args[2:]);
-		if *name =="" {
-			fmt.Println("❌ Name is required. Usage: cli-json-manager add -name <name>")
-			return;
-		} 
-		commands.NewManager(*addFile).AddRecord(name)
-
-	case "list":
-		listCmd.Parse(os.Args[2:])
-		commands.NewManager(*listFile).ListRecord()
-
-	case "delete":
-		deleteCmd.Parse(os.Args[2:])
-			if  *deleteId == 0 {
-			fmt.Println("❌ ID is required. Usage: cli-json-manager delete -id <id>")
-			return;
+		addCmd := flag.NewFlagSet("add",flag.ExitOnError);
+		name := addCmd.String("name","","Name to add");
+		addCmd.Parse(flag.Args()[1:]) // parse arguments *after* add
+		if *name == "" {
+			fmt.Println("❌ missing required flag: --name")
+			os.Exit(2)
 		}
-		commands.NewManager(*deleteFile).DeleteRecord(*deleteId)
+	mgr,err := commands.NewManager(*fileFlag)
+	if err != nil {
+		fmt.Println("❌ Error creating manager:", err)
+		os.Exit(3)
 
-	case "update":
-		updateCmd.Parse(os.Args[2:])
-			if  *updateId == 0 {
-			fmt.Println("❌ ID is required. Usage: cli-json-manager update -id <id>")
-			return;
+	}
+	 if err:=mgr.AddRecord(name); err != nil {
+		fmt.Println( err)
+		os.Exit(4)
+	 }
+
+	//  ---------------- LIST ----------------
+	case "list":
+		listCmd := flag.NewFlagSet("list",flag.ExitOnError);
+		listCmd.Parse(flag.Args()[1:])
+		mgr,err := commands.NewManager(*fileFlag)
+		if err != nil {
+			fmt.Println("❌ Error creating manager:", err)
+			os.Exit(3)
+		}
+		if err := mgr.ListRecord(); err != nil {
+			fmt.Println( err)
+			os.Exit(3)
+		}
+
+		//  ---------------- DELETE ----------------
+		case "delete":
+			deleteCmd:= flag.NewFlagSet("delete",flag.ExitOnError);
+			deleteId :=deleteCmd.Int("id",0,"Id to delete");
+			deleteCmd.Parse(flag.Args()[1:])
+			mgr,err := commands.NewManager(*fileFlag)
+			if err != nil {
+				fmt.Println("❌ Error creating manager:", err)
+				os.Exit(3)
+			}
+			if err := mgr.DeleteRecord(*deleteId); err != nil {
+				fmt.Println( err)
+				os.Exit(4)
 			}
 
-			if *name =="" {
-			fmt.Println("❌ Name is required. Usage: cli-json-manager update -name <name>")
-			return;
-		
-		}
-		commands.NewManager(*updateFile).UpdateRecord(*updateId,*updateName)
+		//  ---------------- UPDATE ----------------
+		case "update":
+			updateCmd := flag.NewFlagSet("update",flag.ExitOnError);
+			updateId := updateCmd.Int("id",0,"Id to update");
+			updateName := updateCmd.String("name","","New Name");
+			updateCmd.Parse(flag.Args()[1:])
+			
 
-	default:
-		fmt.Println("❌ Unknown command: ",os.Args[1],	" Available commands: add, list, delete, update")
+			mgr,err := commands.NewManager(*fileFlag)
+			if err != nil {
+				fmt.Println("❌ Error creating manager:", err)
+				os.Exit(3)
+			}
+			if err := mgr.UpdateRecord(*updateId,*updateName); err != nil {
+				fmt.Println( err)
+				os.Exit(4)
+			}
+
+		//  ---------------- DEFAULT/UNKNOWN COMMAND ----------------
+		default:
+			fmt.Println("❌ Unknown command: ", cmd)
+			fmt.Println("Available commands: add, list, delete, update")
+			os.Exit(1)	
+
 	}
 }
+
+
+/*
+Exit codes:
+0 -> ✅success
+1 -> ❌ general error (missing command, unknown command)
+2 -> ❌ Bad usage, incorrect command syntax [command-specific error (missing flags, invalid input)]
+3 -> ❌ Runtime error [manager creation error (file issues, etc.)]
+4 -> ❌ Operation-specific error (add, delete, update failures)
+*/
--- internal/cli/commands/commands.go
+++ internal/cli/commands/commands.go
@@ -2,28 +2,41 @@ package commands
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/thissidemayur/cli-json-manager/internal/types"
 )
 
+// sentimental error (resuable constant)
+var (
+	ErrNotFound = errors.New("record not found")
+	ErrInvalidId = errors.New("invalid ID")
+	ErrFileNotFound = errors.New("file not found")
+	ErrEmptyName = errors.New("name cannot be empty")
+)
 // Manager struct to handle records
 type Manager struct {
 	fileName string
 }
 
 // NewManager creates a new Manager instance(constructor)
-func NewManager(fileName string) *Manager {
-	return &Manager{fileName: fileName}
+func NewManager(fileName string) (*Manager ,error ){
+	m:= &Manager{fileName: fileName}
+	if err := m.EnsureFile(); err != nil {
+		return nil, fmt.Errorf("ensure file failed: %w", err)
+	}
+	return m, nil
 }
 
+//  ========================== CRUD on File  ==========================
+
 // add a new record
-func (m *Manager) AddRecord(name *string) {
+func (m *Manager) AddRecord(name *string) error {
 	records, err := m.ReadRecord()
 	if err != nil {
-		fmt.Println("❌ Error reading records:", err)
-		return
+		return fmt.Errorf("❌ Error reading records: %w", err)
 	}
 
 	newRecord := types.Record{
@@ -34,35 +47,41 @@ func (m *Manager) AddRecord(name *string) {
 	records = append(records, newRecord)
 
 	// marshal (convert go to json)
-	m.SaveRecords(records)
+	if err := m.SaveRecords(records); err != nil {
+		return err
+	}
 
-	fmt.Println("✅ Record added successfully!")
+	fmt.Printf("✅ Added Record: \"Id: %d, Name: %q\" successfully!\n", newRecord.ID, newRecord.Name)
+	return nil
 }
 
 // read all records
-func (m* Manager) ListRecord(){
-	 records, err := m.ReadRecord()
+func (m *Manager) ListRecord() error {
+	records, err := m.ReadRecord()
 	 if err != nil {
-		 fmt.Println("❌ Error reading records:", err)
-		 return
+		 return err
 	 }
 	 if len(records) == 0 {
-		 fmt.Println("ℹ️ No records found in ", m.fileName)
-		 return
+		 fmt.Printf("ℹ️ No records found in %q\n	", m.fileName)
+		 return nil
 	 }
 
 	 fmt.Println("📋 List of Records:")
 	 for _, record := range records {
 		 fmt.Printf("  - ID: %d, Name: %s\n", record.ID, record.Name)
-	 }	
+	 }
+	 return nil
 }
 
 // delete  a record
-func (m *Manager) DeleteRecord(id int){	
-records, err := m.ReadRecord()
+func (m *Manager) DeleteRecord(id int) error {
+	if id <= 0 {
+		return ErrInvalidId
+	}
+
+	records, err := m.ReadRecord()
 	if err != nil {
-		fmt.Println("❌ Error reading records:", err)
-		return
+return err
 	}
 
 	found := false
@@ -77,8 +96,7 @@ records, err := m.ReadRecord()
 	}
 
 	if !found {
-		fmt.Printf("⚠️  No record found with ID %d\n", id)
-		return
+		 return ErrNotFound
 	}
 
 	// reassign IDs (so they remain sequential)
@@ -90,39 +108,46 @@ records, err := m.ReadRecord()
 	m.SaveRecords(newRecord)
 
 	fmt.Printf("🗑️  Deleted record with ID %d\n", id)
+	return nil
 }
 
 // update a record
-func (m *Manager) UpdateRecord(id int, newName string) {
+func (m *Manager) UpdateRecord(id int, newName string) error {
+	if id == 0 {
+		return ErrInvalidId
+	}
+	if newName == "" {
+		return ErrEmptyName
+	}
+
 	records, err := m.ReadRecord()
 	if err != nil {
-		fmt.Println("❌ Error reading records:", err)
-		return
+		return err
 	}
-	if len(records) == 0 {
-		fmt.Println(" 📂No records found.", m.fileName)
-		return
-	}
+	
 	found := false
 	for i, r := range records {
 		if r.ID == id {
 			found = true
 			records[i].Name = newName
-			return
+			break
 		}
 	}
 
 	if !found {
-		fmt.Printf("⚠️  No record found with ID %d\n", id)
-		return
+		return ErrNotFound
 	}
 	
 	// marshal (convert go to json)
-	m.SaveRecords(records)
+	if err:=m.SaveRecords(records); err != nil {
+		return err
+	}
 	fmt.Printf("✏️  Updated record ID %d → New Name: %s\n", id, newName)
-
+	return nil
 }
 
+// ========================== HELPER Functions ==========================
+ 
 // ensure file exists
 func (m *Manager) EnsureFile() error {
 	_, err := os.Stat(m.fileName)
@@ -132,7 +157,7 @@ func (m *Manager) EnsureFile() error {
 			return err
 		}
 		defer file.Close()
-		file.Write([]byte("[]")) // initialize with empty JSON array	
+		file.Write([]byte("[]")) // initialize  empty JSON array	
 	}
 	return nil
 }
@@ -146,21 +171,19 @@ func (m *Manager) ReadRecord() ([]types.Record , error) {
 	// get file data	
 	data, err := os.ReadFile(m.fileName)
 	if err != nil {
-		fmt.Println("❌ Error reading file:", err)
-		return nil, err
+		return nil, fmt.Errorf("❌ Error reading file: %w", err)
 	}
 
 	var people []types.Record
 	// unmarshal (convert json to go)
 	if err := json.Unmarshal(data, &people); err != nil {
-		fmt.Println("❌ Error unmarshalling data:", err)
-		return nil, err
+		return nil, fmt.Errorf("❌ Invalid JSON format: %w", err)
 	}
 	return people, nil
 
 }
 
-
+// save records to file [ convert go to json and write to file]
 func (m *Manager) SaveRecords(records []types.Record) error {
 	// marshal (convert go to json)
 	data, err := json.MarshalIndent(records, "", " ")
@@ -171,8 +194,7 @@ func (m *Manager) SaveRecords(records []types.Record) error {
 	
 	// write to json file
 	if err := os.WriteFile(m.fileName, data, 0644); err != nil {
-		fmt.Println("❌ Error writing file:", err)
-		return err
+		return fmt.Errorf("❌ Error writing file: %w", err)
 	}
 	return nil
 }
